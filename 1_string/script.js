038acce3b3e03d92725ea15d60ecf68cfc8e37a2
Camila Fonte <kamilafonte@gmail.com>
2020-04-11 16:52:06 -0300

    Crud sessão de strings

---
+++ 1_string/script.js
@@ -0,0 +1,5 @@
+console.log(typeof "Teste string");
+console.log('Aspas simples');
+console.log(`Crase`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ 2_string_avancado/script.js
@@ -0,0 +1,6 @@
+// \n quebra linha
+console.log("Primeira linha \n Segunda linha");
+console.log("Curso oferecido na 'Udemy'");
+console.log('Curso oferecido na "Udemy"');
+console.log(`Multiplicação de 15*5 utilizando backtick ${15*5}`);
+console.log("Concatenando" + " strings")
