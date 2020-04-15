23c5b999076b186a9cfef0d9cf414f0129b40eac
Camila Fonte <kamilafonte@gmail.com>
2020-04-15 17:35:23 -0300

    Crud de exercicios de string

---
+++ 13_exerciciosS1/exerciciosString/script.js
@@ -0,0 +1,15 @@
+// aspas dupla
+console.log("Primeiro exercício referente a sessão string");
+console.log("JS é uma linguagem de programação voltada para o frontend");
+
+// aspas simples
+console.log('Próximo passo será estudar PHP');
+console.log('E estudar banco de dados');
+
+// template literal
+console.log("Concatenando" + " essa frase");
+console.log("Quebrando linha utilizando barra N. \nSegunda linha");
+
+// usando backtick
+console.log(`Primeira linha`);
+console.log(`Backtick com \nbarra N`);
