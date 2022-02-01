fa592fb60433aeda9b8018971ee25b37d621c623
clfonte <kamilafonte@gmail.com>
2022-02-01 16:40:06 -0300

    Declarando variáveis

---
+++ declarandoVariaveis/script.js
@@ -0,0 +1,19 @@
+let nome = "Camila";
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let laranjas = 5;
+console.log(laranjas / laranjas);
+
+nome = "Kageyama";
+console.log(nome);
+
+laranja = 123;
+console.log(laranja);
+
+laranja = "Laranja";
+console.log(laranja);
+
+let um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
