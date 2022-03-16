1b942688ed4a37f51e794a1bf6c3313040d282d8
clfonte <kamilafonte@gmail.com>
2022-03-16 17:02:51 -0300

    Variaveis e palavras reservadas

---
+++ convencao/script.js
@@ -0,0 +1,7 @@
+let nome1 = "Camila";
+let $nome1 = "Camila";
+let _nome1 = "Camila";
+let Nome = "Camila";
+let meuNome = "Camila";
+
+// let @nome = "Camila";
---
+++ declarandoVariaveis_2/script.js
@@ -0,0 +1,12 @@
+var nome = "Camila";
+
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+// uma constante nao consegue ser alterado para outro valor depois de ser declarada
+ip = "123.0.0.1";
+
+// nao pode começar uma variavel com numero
+// alem de ? _ outros caracteres nao sao aceitaveis
